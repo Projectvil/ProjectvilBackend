d9041c7b893681a28cee711a70406874f217ee3b
AlexeyKidA <zhuraulioualiaksei@gmail.com>
2023-06-18 01:15:18 +0300

    cidi dev test

--- WebGateway/Controllers/AuthController.cs
+++ WebGateway/Controllers/AuthController.cs
@@ -20,6 +20,8 @@ public class AuthController : ControllerBase
     public async Task<IActionResult> Login(string email, string password)
     {
         _logger.LogWarning(message: $"Login endpoint: email:{email}, password:{password}");
+        _logger.LogWarning("DEV CIDI!");
+
 
         TokenResponse result = new TokenResponse();
         
